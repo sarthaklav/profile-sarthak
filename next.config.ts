eca5d9d825922c4dc20c44e07cc6243d9a8f1b45
Sarthak Lav <sarthakchaudhary52@gmail.com>
2025-03-14 17:42:39 +0530

    feat: create home page for profile

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  reactStrictMode: true,
+  output: "export",
+  distDir: "dist",
+  trailingSlash: true,
+  assetPrefix: ".",
 };
 
 export default nextConfig;
